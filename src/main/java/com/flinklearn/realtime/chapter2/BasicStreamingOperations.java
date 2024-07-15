36f9ba8e5553faf831b670077c9424fb51e8e882
Matt Braymer-Hayes <matt.braymerhayes@nbcuni.com>
2024-07-15 11:28:52 -0400

    Initial commit.
    * Update pom.xml for Flink 1.18.
    * Update part of chapter 2.
    * Add .gitignore
    * Move remaining files over unchanged.

---
+++ src/main/java/com/flinklearn/realtime/chapter2/BasicStreamingOperations.java
@@ -0,0 +1,125 @@
+package com.flinklearn.realtime.chapter2;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import com.flinklearn.realtime.common.MapCountPrinter;
+import com.flinklearn.realtime.common.Utils;
+import com.flinklearn.realtime.datasource.FileStreamDataGenerator;
+import org.apache.commons.io.FileUtils;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.file.sink.FileSink;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+/*
+A Flink Program that reads a files stream, computes a Map and Reduce operation,
+and writes to a file output
+ */
+
+public class BasicStreamingOperations {
+
+    public static void main(String[] args) {
+        /*
+         *                 Setup Flink environment.
+         */
+
+        // Set up the streaming execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // Keeps the ordering of records. Else multiple threads can change
+        // sequence of printing.
+        env.setParallelism(1);
+
+        /*
+         *                  Read CSV File Stream into a DataStream.
+         */
+
+        // Define the data directory to monitor for new files
+        final String dataDir = "data/raw_audit_trail";
+
+        // Define the text input source
+        final FileSource<String> auditTrailSource = FileSource
+                .forRecordStreamFormat(new TextLineInputFormat(), new Path(dataDir))
+                .monitorContinuously(Duration.ofSeconds(60L)) //monitor interval
+                .build();
+
+        // Create a DataStream based on the source
+        final DataStream<String> auditTrailStream = env.fromSource(
+                auditTrailSource,
+                WatermarkStrategy.noWatermarks(),
+                "file-source"
+        );
+
+        // Convert each record to an Object
+        final DataStream<AuditTrail> auditTrailObj = auditTrailStream
+                .map((MapFunction<String, AuditTrail>) auditStr -> {
+                    System.out.println("--- Received Record : " + auditStr);
+                    return new AuditTrail(auditStr);
+                });
+
+        /*
+         *                  Perform computations and write to output sink.
+        */
+
+        // Print message for audit trail counts
+        MapCountPrinter.printCount(
+                auditTrailObj.map( i -> i),
+                "Audit Trail : Last 5 secs");
+
+        // Window by 5 seconds, count #of records and save to output
+       final DataStream<Tuple2<String,Integer>> recCount
+                = auditTrailObj
+                .map(i -> new Tuple2<>(String.valueOf(System.currentTimeMillis()), 1))
+                .windowAll(TumblingEventTimeWindows.of(Time.seconds(5)))
+                .reduce((x,y) -> new Tuple2<>(x.f0, x.f1 + y.f1));
+
+
+        // Define the output directory to store summary information
+        final String outputDir = "data/five_sec_summary";
+        // Clean out existing files in the directory
+        try {
+            FileUtils.cleanDirectory(new File(outputDir));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Set up a streaming file sink to the output directory
+        final FileSink<Tuple2<String,Integer>> countSink = FileSink
+                .forRowFormat(
+                        new Path(outputDir),
+                        new SimpleStringEncoder<Tuple2<String,Integer>>("UTF-8")
+                ).build();
+
+        // Add the file sink as sink to the DataStream.
+        recCount.sinkTo(countSink);
+
+
+        /*
+         *                  Setup data source and execute the Flink pipeline
+         */
+        // Start the File Stream generator on a separate thread
+        Utils.printHeader("Starting File Data Generator...");
+        Thread genThread = new Thread(new FileStreamDataGenerator());
+        genThread.start();
+
+        genThread.interrupt();
+
+        // execute the streaming pipeline
+        try {
+            env.execute("Flink Streaming Audit Trail Example");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/com/flinklearn/realtime/datasource/FileStreamDataGenerator.java
@@ -0,0 +1,123 @@
+package com.flinklearn.realtime.datasource;
+
+import com.opencsv.CSVWriter;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/****************************************************************************
+ * This Generator generates a series of data files in the raw_data folder
+ * It is an audit trail data source.
+ * This can be used for streaming consumption of data by Flink
+ ****************************************************************************/
+
+public class FileStreamDataGenerator implements Runnable {
+
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+
+    public static void main(String[] args) {
+        FileStreamDataGenerator fsdg = new FileStreamDataGenerator();
+        fsdg.run();
+    }
+
+    public void run() {
+        //Define list of users
+        List<String> appUser = new ArrayList<>();
+        appUser.add("Tom");
+        appUser.add("Harry");
+        appUser.add("Bob");
+
+        //Define list of application operations
+        List<String> appOperation = new ArrayList<>();
+        appOperation.add("Create");
+        appOperation.add("Modify");
+        appOperation.add("Query");
+        appOperation.add("Delete");
+
+        //Define list of application entities
+        List<String> appEntity = new ArrayList<>();
+        appEntity.add("Customer");
+        appEntity.add("SalesRep");
+
+        //Define the data directory to output the files
+        String dataDir = "data/raw_audit_trail";
+
+        //Clean out existing files in the directory
+        try {
+            FileUtils.cleanDirectory(new File(dataDir));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Define a random number generator
+        Random random = new Random();
+
+        //Generate 100 sample audit records, one per each file
+        for(int i=0; i < 100; i++) {
+
+            //Capture current timestamp
+            final String currentTime = String.valueOf(System.currentTimeMillis());
+
+            //Generate a random user
+            final String user = appUser.get(random.nextInt(appUser.size()));
+            //Generate a random operation
+            final String operation = appOperation.get(random.nextInt(appOperation.size()));
+            //Generate a random entity
+            final String entity = appEntity.get(random.nextInt(appEntity.size()));
+            //Generate a random duration for the operation
+            final String duration = String.valueOf(random.nextInt(10) + 1 );
+            //Generate a random value for number of changes
+            final String changeCount = String.valueOf(random.nextInt(4) + 1);
+
+            //Create a CSV Text array
+            final String[] csvText = { String.valueOf(i), user, entity,
+                                    operation, currentTime, duration, changeCount} ;
+
+            //Open a new file for this record
+            FileWriter auditFile;
+            try {
+                auditFile = new FileWriter(dataDir
+                        + "/audit_trail_" + i + ".csv");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            final CSVWriter auditCSV = new CSVWriter(auditFile);
+
+            //Write the audit record and close the file
+            auditCSV.writeNext(csvText);
+
+            System.out.println(ANSI_BLUE + "FileStream Generator : Creating File : "
+                        + Arrays.toString(csvText) + ANSI_RESET);
+
+            try {
+                auditCSV.flush();
+                auditCSV.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            //Sleep for a random time ( 1 - 3 secs) before the next record.
+            try {
+                Thread.sleep(random.nextInt(2000) + 1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+
+    }
+
+
+}
